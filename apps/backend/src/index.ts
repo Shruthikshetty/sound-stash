d86e87cb37f8adde4105c0eead0aed6b1ca037b3
shruthik shetty <shrutikshetty14@gmail.com>
2026-08-22 21:49:16 +0530

    add swagger doc config

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,8 +1,12 @@
 import createApp from "./lib/create-app";
+import configureOpenApi from "./lib/open-api-config";
 import base from "./routes/index";
 
 // create app
 const app = createApp();
+
+//configure open api
+configureOpenApi(app);
 
 // all routes go here
 const routes = [base];
