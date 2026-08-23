a7c46b2ca656d3da48dd46789d27ec07dac0e02e
shruthik shetty <shrutikshetty14@gmail.com>
2026-08-23 23:16:33 +0530

    fix

--- apps/backend/src/middleware/not-found.ts
+++ apps/backend/src/middleware/not-found.ts
@@ -6,6 +6,7 @@ import { UNPROCESSABLE_ENTITY } from "shared/constants";
 const handleNotFound: NotFoundHandler = (c) => {
   return c.json(
     {
+      success: false,
       message: "route not found",
     },
     UNPROCESSABLE_ENTITY,
